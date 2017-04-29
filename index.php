e82fe62e177b7046fd32c130839842028b610beb
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2017-04-30 04:05:19 +0530

    connection check1

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
-    $user='root';
-    $pass='';
+    $connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
 
-    $dbname='dbms-project';
-    $conn = new mysqli('localhost',$user,$pass,$dbname) or die("Connection failed");
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+  // echo $connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname;
+  
+$conn = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
 ?> 
 <!DOCTYPE html>
 <html>
@@ -65,7 +78,7 @@
           </tr>
             <?php 
             $sql = "SELECT * FROM ".$classview." order by date ";
-            $result = $conn->query($sql);
+            $result = mysqli_query($conn,$sql);
             while($row=mysqli_fetch_assoc($result)){
             echo "
                     <tr>
